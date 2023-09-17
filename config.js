f2c37deb662ddd9ef2290fc3578565a7ee3c0fa8
Hajdu Zoltán <j9322l@inf.elte.hu>
2023-09-17 18:33:21 +0200

    initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://self-host-movie-web.h-zoltannn.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3Y2M0NDRhM2Y2OGUwMWU5OThkM2IyNTBhZDIxZDY4YiIsInN1YiI6IjY1MDcxOTVmM2NkMTJjMDBjYTU1ZDk0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RWtWtFyvU3FHV39REHml4brZjGUBMZzO0QArEHZO0SQ"
+};
